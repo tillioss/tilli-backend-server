fc660a57ade25a20737815a840661b4a949097c3
priyanthabuddhika <priyanthabuddhika@gmail.com>
2025-05-23 21:40:15 +0530

    test: add unit tests for ResultAccumulator

--- src/test/scala/com/teqbahn/actors/analytics/result/ResultAccumulatorSpec.scala
+++ src/test/scala/com/teqbahn/actors/analytics/result/ResultAccumulatorSpec.scala
@@ -1,18 +1,16 @@
 package com.teqbahn.actors.analytics.result
 
+import com.teqbahn.bootstrap.StarterMain
+import com.teqbahn.caseclasses._
+import com.teqbahn.global.ZiRedisCons
+import io.lettuce.core.api.sync.RedisCommands
 import org.apache.pekko.actor.{ActorSystem, Props}
-import org.apache.pekko.testkit.{ImplicitSender, TestKit, TestProbe}
+import org.apache.pekko.testkit.{ImplicitSender, TestKit}
+import org.mockito.ArgumentMatchers.anyString
+import org.mockito.Mockito._
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpecLike
-import com.teqbahn.caseclasses._
-import com.teqbahn.bootstrap.StarterMain
-import com.teqbahn.global.ZiRedisCons
-import java.time.LocalDateTime
-import org.joda.time.DateTime
-import io.lettuce.core.api.sync.RedisCommands
-import org.mockito.Mockito._
-import org.mockito.ArgumentMatchers.anyString
 
 class ResultAccumulatorSpec
     extends TestKit(ActorSystem("ResultAccumulatorSpec"))
@@ -41,7 +39,7 @@ class ResultAccumulatorSpec
       when(mockRedis.get(ZiRedisCons.ACCUMULATOR_LanguageUserCounter + "english")).thenReturn("20")
       when(mockRedis.get(ZiRedisCons.ACCUMULATOR_AgeUserCounter + "7")).thenReturn("5")
 
-      // Create and send request
+      // Create and send a request
       val request = FetchAnalyticsRequest(id = "test-request-1")
       resultAccumulator ! request
 
@@ -157,5 +155,367 @@ class ResultAccumulatorSpec
       // Should contain default "0" values
       responseJson should include("\"y\":\"0\"")
     }
+
+    "handle FetchFilterAnalyticsRequest with filter requestType correctly" in {
+      val resultAccumulator = system.actorOf(Props[ResultAccumulator])
+
+      val testDate = "2024-03-20"
+      when(mockRedis.get(ZiRedisCons.ACCUMULATOR_FILTER_SEPARATOR + testDate + "::age_7")).thenReturn("10")
+      when(mockRedis.get(ZiRedisCons.ACCUMULATOR_FILTER_SEPARATOR + testDate + "::gender_male::lang_english")).thenReturn("15")
+      when(mockRedis.get(ZiRedisCons.ACCUMULATOR_FILTER_SEPARATOR + testDate + "::age_7::gender_male")).thenReturn("20")
+      when(mockRedis.get(ZiRedisCons.ACCUMULATOR_FILTER_SEPARATOR + testDate + "::age_7::lang_english")).thenReturn("25")
+      when(mockRedis.get(ZiRedisCons.ACCUMULATOR_FILTER_SEPARATOR + testDate + "::age_7::gender_male::lang_english")).thenReturn("30")
+
+      val request = FetchFilterAnalyticsRequest(
+        sDate = Some(testDate),
+        eDate = Some(testDate),
+        filterGender = List("male"),
+        filterLanguage = List("english"),
+        filterAge = List("7"),
+        requestType = "filter",
+        id = "test-filter-request"
+      )
+
+      resultAccumulator ! request
+
+      val response = expectMsgType[FetchFilterAnalyticsResponse]
+      val responseJson = response.response
+
+      responseJson should include("dateBased")
+      responseJson should include(testDate)
+      responseJson should include("\"y\":\"30\"")
+    }
+
+    "handle FetchFilterAnalyticsRequest with filter requestType and only age filter" in {
+      val resultAccumulator = system.actorOf(Props[ResultAccumulator])
+
+      val testDate = "2024-03-20"
+      when(mockRedis.get(ZiRedisCons.ACCUMULATOR_FILTER_SEPARATOR + testDate + "::age_7")).thenReturn("10")
+
+      val request = FetchFilterAnalyticsRequest(
+        sDate = Some(testDate),
+        eDate = Some(testDate),
+        filterGender = List(),
+        filterLanguage = List(),
+        filterAge = List("7"),
+        requestType = "filter",
+        id = "test-age-only-request"
+      )
+
+      resultAccumulator ! request
+
+      val response = expectMsgType[FetchFilterAnalyticsResponse]
+      val responseJson = response.response
+
+      responseJson should include("dateBased")
+      responseJson should include(testDate)
+      responseJson should include("\"y\":\"10\"")
+    }
+
+    "handle FetchFilterAnalyticsRequest with filter requestType and only language filter" in {
+      val resultAccumulator = system.actorOf(Props[ResultAccumulator])
+
+      val testDate = "2024-03-20"
+      when(mockRedis.get(ZiRedisCons.ACCUMULATOR_FILTER_SEPARATOR + testDate + "::lang_english")).thenReturn("15")
+
+      val request = FetchFilterAnalyticsRequest(
+        sDate = Some(testDate),
+        eDate = Some(testDate),
+        filterGender = List(),
+        filterLanguage = List("english"),
+        filterAge = List(),
+        requestType = "filter",
+        id = "test-language-only-request"
+      )
+
+      resultAccumulator ! request
+
+      val response = expectMsgType[FetchFilterAnalyticsResponse]
+      val responseJson = response.response
+
+      responseJson should include("dateBased")
+      responseJson should include(testDate)
+      responseJson should include("\"y\":\"15\"")
+    }
+
+    "handle FetchFilterAnalyticsRequest with filter requestType and only gender filter" in {
+      val resultAccumulator = system.actorOf(Props[ResultAccumulator])
+
+      val testDate = "2024-03-20"
+      when(mockRedis.get(ZiRedisCons.ACCUMULATOR_FILTER_SEPARATOR + testDate + "::gender_male")).thenReturn("20")
+
+      val request = FetchFilterAnalyticsRequest(
+        sDate = Some(testDate),
+        eDate = Some(testDate),
+        filterGender = List("male"),
+        filterLanguage = List(),
+        filterAge = List(),
+        requestType = "filter",
+        id = "test-gender-only-request"
+      )
+
+      resultAccumulator ! request
+
+      val response = expectMsgType[FetchFilterAnalyticsResponse]
+      val responseJson = response.response
+
+      responseJson should include("dateBased")
+      responseJson should include(testDate)
+      responseJson should include("\"y\":\"20\"")
+    }
+
+    "handle FetchFilterAnalyticsRequest with filter requestType and combined age+language filter" in {
+      val resultAccumulator = system.actorOf(Props[ResultAccumulator])
+
+      val testDate = "2024-03-20"
+      when(mockRedis.get(ZiRedisCons.ACCUMULATOR_FILTER_SEPARATOR + testDate + "::age_7::lang_english")).thenReturn("25")
+
+      val request = FetchFilterAnalyticsRequest(
+        sDate = Some(testDate),
+        eDate = Some(testDate),
+        filterGender = List(),
+        filterLanguage = List("english"),
+        filterAge = List("7"),
+        requestType = "filter",
+        id = "test-age-language-request"
+      )
+
+      resultAccumulator ! request
+
+      val response = expectMsgType[FetchFilterAnalyticsResponse]
+      val responseJson = response.response
+
+      responseJson should include("dateBased")
+      responseJson should include(testDate)
+      responseJson should include("\"y\":\"25\"")
+    }
+
+    "handle FetchFilterAnalyticsRequest with filter requestType and combined gender+language filter" in {
+      val resultAccumulator = system.actorOf(Props[ResultAccumulator])
+
+      val testDate = "2024-03-20"
+      when(mockRedis.get(ZiRedisCons.ACCUMULATOR_FILTER_SEPARATOR + testDate + "::gender_male::lang_english")).thenReturn("30")
+
+      val request = FetchFilterAnalyticsRequest(
+        sDate = Some(testDate),
+        eDate = Some(testDate),
+        filterGender = List("male"),
+        filterLanguage = List("english"),
+        filterAge = List(),
+        requestType = "filter",
+        id = "test-gender-language-request"
+      )
+
+      resultAccumulator ! request
+
+      val response = expectMsgType[FetchFilterAnalyticsResponse]
+      val responseJson = response.response
+
+      responseJson should include("dateBased")
+      responseJson should include(testDate)
+      responseJson should include("\"y\":\"30\"")
+    }
+
+    "handle FetchFilterAnalyticsRequest with filter requestType and combined age+gender filter" in {
+      val resultAccumulator = system.actorOf(Props[ResultAccumulator])
+
+      val testDate = "2024-03-20"
+      when(mockRedis.get(ZiRedisCons.ACCUMULATOR_FILTER_SEPARATOR + testDate + "::age_7::gender_male")).thenReturn("35")
+
+      val request = FetchFilterAnalyticsRequest(
+        sDate = Some(testDate),
+        eDate = Some(testDate),
+        filterGender = List("male"),
+        filterLanguage = List(),
+        filterAge = List("7"),
+        requestType = "filter",
+        id = "test-age-gender-request"
+      )
+
+      resultAccumulator ! request
+
+      val response = expectMsgType[FetchFilterAnalyticsResponse]
+      val responseJson = response.response
+
+      responseJson should include("dateBased")
+      responseJson should include(testDate)
+      responseJson should include("\"y\":\"35\"")
+    }
+
+    "test getOptValue utility method through result checking" in {
+      val resultAccumulator = system.actorOf(Props[ResultAccumulator])
+
+      val validDate = "2024-03-20"
+      when(mockRedis.get(anyString())).thenReturn("0")
+
+      val requestWithValidDate = FetchFilterAnalyticsRequest(
+        sDate = Some(validDate),
+        eDate = Some(validDate),
+        filterGender = List(),
+        filterLanguage = List(),
+        filterAge = List(),
+        requestType = "normal",
+        id = "test-valid-date"
+      )
+
+      resultAccumulator ! requestWithValidDate
+      val validResponse = expectMsgType[FetchFilterAnalyticsResponse]
+
+      validResponse.response should include("dateBased")
+      validResponse.response should include(validDate)
+
+      val requestWithNoneDate = FetchFilterAnalyticsRequest(
+        sDate = None,
+        eDate = Some(validDate),
+        filterGender = List(),
+        filterLanguage = List(),
+        filterAge = List(),
+        requestType = "normal",
+        id = "test-none-date"
+      )
+
+      resultAccumulator ! requestWithNoneDate
+      val responseWithNoneDate = expectMsgType[FetchFilterAnalyticsResponse]
+
+      responseWithNoneDate.response should be("""{"dateBased":[],"dateBasedGender":[],"dateBasedLanguage":[]}""")
+    }
+
+    "test invalid date handling in requests" in {
+      val resultAccumulator = system.actorOf(Props[ResultAccumulator])
+
+      val requestWithBothInvalid = FetchFilterAnalyticsRequest(
+        sDate = None,
+        eDate = None,
+        filterGender = List(),
+        filterLanguage = List(),
+        filterAge = List(),
+        requestType = "normal",
+        id = "test-both-invalid"
+      )
+
+      resultAccumulator ! requestWithBothInvalid
+      val responseWithBothInvalid = expectMsgType[FetchFilterAnalyticsResponse]
+
+      responseWithBothInvalid.response should be("""{"dateBased":[],"dateBasedGender":[],"dateBasedLanguage":[]}""")
+    }
+
+    "test formatted dates in different months" in {
+      val resultAccumulator = system.actorOf(Props[ResultAccumulator])
+
+      val startDate = "2024-01-15"
+      val endDate = "2024-01-17"
+
+      // Mock Redis for these dates
+      when(mockRedis.get(ZiRedisCons.ACCUMULATOR_DayUserCounter + "2024-01-15")).thenReturn("10")
+      when(mockRedis.get(ZiRedisCons.ACCUMULATOR_DayUserCounter + "2024-01-16")).thenReturn("20")
+      when(mockRedis.get(ZiRedisCons.ACCUMULATOR_DayUserCounter + "2024-01-17")).thenReturn("30")
+      when(mockRedis.get(anyString())).thenReturn(null)
+
+      val request = FetchFilterAnalyticsRequest(
+        sDate = Some(startDate),
+        eDate = Some(endDate),
+        filterGender = List(),
+        filterLanguage = List(),
+        filterAge = List(),
+        requestType = "normal",
+        id = "test-formatted-dates"
+      )
+
+      resultAccumulator ! request
+      val response = expectMsgType[FetchFilterAnalyticsResponse]
+
+      response.response should include("2024-01-15")
+      response.response should include("2024-01-16")
+      response.response should include("2024-01-17")
+    }
+
+    "test double digit formatting through months with single digits" in {
+      val resultAccumulator = system.actorOf(Props[ResultAccumulator])
+
+      val dates = List(
+        "2024-01-05", // Tests single-digit month
+        "2024-05-07", // Tests single-digit day
+        "2024-12-25"  // Tests double-digit month and day
+      )
+
+      // Mock Redis responses for these dates
+      when(mockRedis.get(ZiRedisCons.ACCUMULATOR_DayUserCounter + "2024-01-05")).thenReturn("10")
+      when(mockRedis.get(ZiRedisCons.ACCUMULATOR_DayUserCounter + "2024-05-07")).thenReturn("20")
+      when(mockRedis.get(ZiRedisCons.ACCUMULATOR_DayUserCounter + "2024-12-25")).thenReturn("30")
+      when(mockRedis.get(anyString())).thenReturn(null)
+
+      for (date <- dates) {
+        val request = FetchFilterAnalyticsRequest(
+          sDate = Some(date),
+          eDate = Some(date),
+          filterGender = List(),
+          filterLanguage = List(),
+          filterAge = List(),
+          requestType = "normal",
+          id = s"test-double-digits-$date"
+        )
+
+        resultAccumulator ! request
+        val response = expectMsgType[FetchFilterAnalyticsResponse]
+
+        response.response should include(date)
+      }
+    }
+
+    "handle invalid date range correctly" in {
+      val resultAccumulator = system.actorOf(Props[ResultAccumulator])
+
+      val request = FetchFilterAnalyticsRequest(
+        sDate = None,
+        eDate = None,
+        filterGender = List("male"),
+        filterLanguage = List("english"),
+        filterAge = List("7"),
+        requestType = "normal",
+        id = "test-invalid-dates"
+      )
+
+      resultAccumulator ! request
+
+      val response = expectMsgType[FetchFilterAnalyticsResponse]
+      val responseJson = response.response
+
+      responseJson shouldBe """{"dateBased":[],"dateBasedGender":[],"dateBasedLanguage":[]}"""
+    }
+
+    "handle multiple ages, languages and genders in filter request" in {
+      val resultAccumulator = system.actorOf(Props[ResultAccumulator])
+
+      val testDate = "2024-03-20"
+
+      when(mockRedis.get(ZiRedisCons.ACCUMULATOR_FILTER_SEPARATOR + testDate + "::age_6::gender_male::lang_english")).thenReturn("10")
+      when(mockRedis.get(ZiRedisCons.ACCUMULATOR_FILTER_SEPARATOR + testDate + "::age_7::gender_female::lang_tamil")).thenReturn("20")
+
+      when(mockRedis.get(anyString())).thenReturn(null)
+
+      when(mockRedis.get(ZiRedisCons.ACCUMULATOR_FILTER_SEPARATOR + testDate + "::age_6::gender_male::lang_english")).thenReturn("10")
+      when(mockRedis.get(ZiRedisCons.ACCUMULATOR_FILTER_SEPARATOR + testDate + "::age_7::gender_female::lang_tamil")).thenReturn("20")
+
+      when(mockRedis.get(ZiRedisCons.ACCUMULATOR_FILTER_SEPARATOR + testDate + "::age_6::gender_female::lang_tamil")).thenReturn("30")
+
+      val request = FetchFilterAnalyticsRequest(
+        sDate = Some(testDate),
+        eDate = Some(testDate),
+        filterGender = List("male", "female"),
+        filterLanguage = List("english", "tamil"),
+        filterAge = List("6", "7"),
+        requestType = "filter",
+        id = "test-multiple-filters"
+      )
+
+      resultAccumulator ! request
+
+      val response = expectMsgType[FetchFilterAnalyticsResponse]
+      val responseJson = response.response
+
+      responseJson should include("dateBased")
+      responseJson should include(testDate)
+      responseJson should include("\"y\":\"60\"")
+    }
   }
-} 
+}
